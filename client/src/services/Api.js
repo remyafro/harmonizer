da4a3b724208dba8479d01e4d71e2b4770b4987b
rramli <remy.ramli@hotmail.com>
2019-11-20 23:12:34 +0800

    test

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
-        baseURL: process.env.baseURL || `http://localhost:5000`
+        baseURL: process.env.baseURL
     })
 }
