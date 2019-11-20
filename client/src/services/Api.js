569d125c6f5242fb1bdf91ce2b0b8c016ab7d4cd
rramli <remy.ramli@hotmail.com>
2019-11-20 22:53:09 +0800

    test

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+const config = require('./config/config');
 
 export default () => {
     return axios.create({
-        baseURL: `http://localhost:5000`
+        baseURL: `http://localhost:${config.port}`
     })
 }
